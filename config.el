c5e2a97b0affaa13cd21edc7602fbd82eec36ec4
Jinyi Xian <kylerky@live.com>
2025-04-23 19:01:30 +0800

    Add to the dirvish-mode-map

--- config.el
+++ config.el
@@ -161,6 +161,27 @@
                 :desc "Transclusion from a link"        "L l"  #'org-transclusion-make-from-link
                 :desc "Org Transclusion mode"           "L t"  #'org-transclusion-mode)))
 
+(map! (:when (modulep! :emacs dired +dirvish)
+        :after dirvish
+        :map dirvish-mode-map
+        "a"    #'dirvish-quick-access
+        "f"    #'dirvish-file-info-menu
+        "y"    #'dirvish-yank-menu
+        "N"    #'dirvish-narrow
+        "["    #'dirvish-history-last
+        "h"    #'dirvish-history-jump ; remapped `describe-mode'
+        "s"    #'dirvish-quicksort ; remapped `dired-sort-toggle-or-edit'
+        "v"    #'dirvish-vc-menu   ; remapped `dired-view-file'
+        "TAB"  #'dirvish-subtree-toggle
+        "M-f"  #'dirvish-history-go-forward
+        "M-b"  #'dirvish-history-go-backward
+        "M-l"  #'dirvish-ls-switches-menu
+        "M-m"  #'dirvish-mark-menu
+        "M-t"  #'dirvish-layout-toggle
+        "M-c"  #'dirvish-setup-menu
+        "M-e"  #'dirvish-emerge-menu
+        "M-j"  #'dirvish-fd-jump))
+
 (use-package! org-noter
   :defer
   :custom
