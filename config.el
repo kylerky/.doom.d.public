7853d3bcfe1a9adcd14275c59e7811f72143b4fc
Jinyi Xian <kylerky@live.com>
2025-09-24 10:34:27 +0800

    Disable rust-mode temporarily

--- config.el
+++ config.el
@@ -314,15 +314,17 @@
                                         (variable (styles +orderless-with-initialism partial-completion))
                                         (symbol (styles +orderless-with-initialism partial-completion)))))
 
-(use-package! rust-mode
-  :defer
-  :after flycheck
-  :hook
-  ((rust-mode . eglot-ensure))
-  :custom
-  ((rust-mode-treesitter-derive t)))
+;; (use-package! rust-mode
+;;   :defer
+;;   :after flycheck
+;;   :hook
+;;   ((rust-mode . eglot-ensure))
+;;   :custom
+;;   ((rust-mode-treesitter-derive t)))
 
 (use-package! rust-ts-mode
+  :init
+  (add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-ts-mode))
   :defer
   :after flycheck
   :hook
